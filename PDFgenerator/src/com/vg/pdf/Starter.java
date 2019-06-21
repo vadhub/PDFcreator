811d7f4d936bbf54893194ef4429461c54bc23c3
Vadim <gabderahmanov99@gmail.com>
2019-06-21 16:22:25 +0400

    add convertTXT class

--- PDFgenerator/src/com/vg/pdf/Starter.java
+++ PDFgenerator/src/com/vg/pdf/Starter.java
@@ -1,17 +1,9 @@
 package com.vg.pdf;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.IOException;
 
 public class Starter {
 	
@@ -26,34 +18,6 @@ public class Starter {
 	public static void main(String[] args) throws IOException {
 		
 		cw. PDFconvertor(new File(DOCS), new File("C://Users/Вадим/Desktop/Hello.pdf"));
-		
-		Document document = new Document();
-		File file = new File(DEST);
-		
-        file.getParentFile().mkdirs();
-	      try
-	      {
-	        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(DEST));
-	        BufferedReader br = new BufferedReader(new FileReader(TEXT));
-	        
-	        document.open();
-	        Paragraph p;
-	        
-	        String strLine;
-	        	   while ((strLine = br.readLine()) != null){	        		   
-	                   p = new Paragraph(strLine);
-	                   document.add(p);
-	        	   }
-	         document.close();
-	         
-	         System.out.println("pdf file created");
-	      } catch (DocumentException e)
-	      {
-	         e.printStackTrace();
-	      } catch (FileNotFoundException e)
-	      {
-	         e.printStackTrace();
-	      }
 	    }	        
 	
 	}
