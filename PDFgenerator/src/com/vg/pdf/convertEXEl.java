c701d09acbdb96e4139f985bfcff65d830130ce1
Vadim <gabderahmanov99@gmail.com>
2019-06-21 17:39:15 +0400

    add convertEXEL class

---
+++ PDFgenerator/src/com/vg/pdf/convertEXEl.java
@@ -0,0 +1,29 @@
+package com.vg.pdf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.documents4j.api.DocumentType;
+import com.documents4j.api.IConverter;
+import com.documents4j.job.LocalConverter;
+
+public class convertEXEl {
+	 public void PDFconvertor(File inputWord, File outputFile){
+	        try  {
+	            InputStream docxInputStream = new FileInputStream(inputWord);
+	            OutputStream outputStream = new FileOutputStream(outputFile);
+	            
+	            IConverter converter = LocalConverter.builder().build();
+	            
+	            converter.convert(docxInputStream).as(DocumentType.MS_EXCEL).to(outputStream).as(DocumentType.PDF).execute();
+	            outputStream.close();
+	            
+	            System.out.println("well done");
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+}
