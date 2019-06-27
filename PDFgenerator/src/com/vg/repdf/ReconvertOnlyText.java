1241c80993bc8c20450a8017ab2ca22a2166e048
Vadim <gabderahmanov99@gmail.com>
2019-06-27 14:54:48 +0400

    add code into convertonlytext class

--- PDFgenerator/src/com/vg/repdf/ReconvertOnlyText.java
+++ PDFgenerator/src/com/vg/repdf/ReconvertOnlyText.java
@@ -1,5 +1,32 @@
 package com.vg.repdf;
 
-public class ReconvertOnlyText {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class ReconvertOnlyText {
+	public void reconvert(String input, String output) throws IOException{
+		PDDocument doc = PDDocument.load(new File(input));
+		PDFTextStripper ts = new PDFTextStripper();
+		int numbOfPage = doc.getNumberOfPages();
+		ts.setStartPage(1);
+		ts.setEndPage(numbOfPage);
+		
+		String text = ts.getText(doc);
+		System.out.println(text);
+		
+		FileOutputStream fos = new FileOutputStream(output);
+		
+		byte[] buf = text.getBytes();
+		
+		fos.write(buf);
+		
+		doc.close();
+		fos.flush();
+		fos.close();
+		System.out.println("WELL DONE!!!!");
+	}
 }
