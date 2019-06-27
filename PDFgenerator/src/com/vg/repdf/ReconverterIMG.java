31c9fcb5ae2f6b0bf27487733f244e902a557674
Vadim <gabderahmanov99@gmail.com>
2019-06-27 14:07:44 +0400

    created repdf package

---
+++ PDFgenerator/src/com/vg/repdf/ReconverterIMG.java
@@ -0,0 +1,26 @@
+package com.vg.repdf;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+public class ReconverterIMG {
+	 public void reconvertor(String inputWord, String outputFile, int page) throws IOException{
+		 File f = new File(inputWord);
+		 PDDocument doc = PDDocument.load(f);
+		 
+		 PDFRenderer ren = new PDFRenderer(doc);
+		 
+		 BufferedImage im = ren.renderImage(page);
+		 ImageIO.write(im, "JPEG", new File(outputFile));
+		 
+		 System.out.println("well done");
+		 
+		 
+	 }
+}
