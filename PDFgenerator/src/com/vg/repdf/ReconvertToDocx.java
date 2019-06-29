85e41cbf35b1beddd42b92ba96c8fd03c0ac90e0
Vadim <gabderahmanov99@gmail.com>
2019-06-29 14:34:42 +0400

    convertasion pdf to word(.docx)

--- PDFgenerator/src/com/vg/repdf/ReconvertToDocx.java
+++ PDFgenerator/src/com/vg/repdf/ReconvertToDocx.java
@@ -1,5 +1,20 @@
 package com.vg.repdf;
 
+import com.spire.pdf.FileFormat;
+import com.spire.pdf.PdfDocument;
+
 public class ReconvertToDocx {
+	
+	public void convert(String input, String output){
+		
+		
+		PdfDocument doc = new PdfDocument();	
+		doc.loadFromFile(input);
+		doc.saveToFile(output, FileFormat.DOCX);
+		doc.close();
+		
+		
+		System.out.println("Easy!!!");
+	}
 
 }
